b042d6c5db89ab3f8a188d1049c868f7ac41169c
Matheus Nunes <matheus.alnun@gmail.com>
2023-03-15 21:45:22 -0300

    Implement bearer token JWT auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersRepository: UsersRepository,
+    private jwtService: JwtService
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto)
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto
+  ): Promise<{ acessToken: string }> {
+    const { username, password } = authCredentialsDto
+
+    const user = await this.usersRepository.findOneBy({ username })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username }
+      const acessToken: string = await this.jwtService.sign(payload)
+      return { acessToken }
+    }
+
+    throw new UnauthorizedException('Please check your login credentials')
   }
 }
