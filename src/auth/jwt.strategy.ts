ea291d5fe4b7fffba35bb55dbe040111d016bb6e
Matheus Nunes <matheus.alnun@gmail.com>
2023-03-17 17:25:13 -0300

    Adiciona chave do JWT ao .env

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { JwtPayload } from "./jwt-payload.interface";
@@ -8,10 +9,11 @@ import { UsersRepository } from "./users.repository";
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor (
-    private usersRepository: UsersRepository
+    private usersRepository: UsersRepository,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret51',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     })
   }
--- src/config.schema.ts
+++ src/config.schema.ts
@@ -2,9 +2,12 @@ import * as Joi from '@hapi/joi'
 
 export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
+  // DB
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432),
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
+  // Auth
+  JWT_SECRET: Joi.string().required(),
 })
